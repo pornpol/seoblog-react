4c346ee10b78f158a688c7854fbadc1903b76f35
pornpol <pornpol.w@gmail.com>
2020-03-19 01:00:14 +0700

    Show all categories and tags

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -16,6 +16,22 @@ const Blogs = ({ blogs, categories, tags, size }) => {
     ));
   };
 
+  const showAllCategories = () => {
+    return categories.map((c, i) => (
+      <Link href={`/categories/${c.slug}`} key={i}>
+        <a className='btn btn-primary mr-1 ml-1 mt-3'>{c.name}</a>
+      </Link>
+    ));
+  };
+
+  const showAllTags = () => {
+    return tags.map((t, i) => (
+      <Link href={`/tags/${t.slug}`} key={i}>
+        <a className='btn btn-outline-primary mr-1 ml-1 mt-3'>{t.name}</a>
+      </Link>
+    ));
+  };
+
   return (
     <Layout>
       <main>
@@ -27,7 +43,11 @@ const Blogs = ({ blogs, categories, tags, size }) => {
               </h1>
             </div>
             <section>
-              <p>show categories and tag</p>
+              <div className='pb-5 text-center'>
+                {showAllCategories()}
+                <br />
+                {showAllTags()}
+              </div>
             </section>
           </header>
         </div>
