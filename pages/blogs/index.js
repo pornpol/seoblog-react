699026ee587a322e72ca270d90aee522fe41776a
pornpol <pornpol.w@gmail.com>
2020-03-19 21:46:40 +0700

    Add SEO for blogs index page

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,12 +1,48 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { withRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import { useState } from 'react';
 import { listBlogsWithCategorisAndTags } from '../../actions/blog';
 
 import Card from '../../components/blog/Card';
 
-const Blogs = ({ blogs, categories, tags, size }) => {
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
+
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+  const head = () => (
+    <Head>
+      <title>Programming blogs | {APP_NAME}</title>
+      <meta
+        name='description'
+        content='Programming blogs and tutorials on react node next vue php laravel ans web development'
+      />
+      <link rel='canonical' href={`${DOMAIN}${router.pathname}`} />
+      <meta
+        property='og:title'
+        content={`Latest web development tutorials | ${APP_NAME}`}
+      />
+      <meta
+        property='og:description'
+        content='Programming blogs and tutorials on react node next vue php laravel ans web development'
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content={`${DOMAIN}${router.pathname}`} />
+      <meta property='og:site_name' content={`${APP_NAME}`} />
+
+      <meta
+        property='og:image'
+        content={`${DOMAIN}/static/images/seoblog.jpg`}
+      />
+      <meta
+        property='og:image:secure_url'
+        content={`${DOMAIN}/static/images/seoblog.jpg`}
+      />
+      <meta property='og:image:type' content='image/jpg' />
+      <meta property='fb:app_id' content={`${FB_APP_ID}`} />
+    </Head>
+  );
+
   const showAllBlogs = () => {
     return blogs.map((blog, i) => (
       <article key={i}>
@@ -33,31 +69,34 @@ const Blogs = ({ blogs, categories, tags, size }) => {
   };
 
   return (
-    <Layout>
-      <main>
-        <div className='container-fluid'>
-          <header>
-            <div className='col-md-12 pt-3'>
-              <h1 className='display-4 font-weight-bold text-center'>
-                Programing blogs and tutorial
-              </h1>
-            </div>
-            <section>
-              <div className='pb-5 text-center'>
-                {showAllCategories()}
-                <br />
-                {showAllTags()}
+    <React.Fragment>
+      {head()}
+      <Layout>
+        <main>
+          <div className='container-fluid'>
+            <header>
+              <div className='col-md-12 pt-3'>
+                <h1 className='display-4 font-weight-bold text-center'>
+                  Programing blogs and tutorial
+                </h1>
               </div>
-            </section>
-          </header>
-        </div>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-md-12'>{showAllBlogs()}</div>
+              <section>
+                <div className='pb-5 text-center'>
+                  {showAllCategories()}
+                  <br />
+                  {showAllTags()}
+                </div>
+              </section>
+            </header>
           </div>
-        </div>
-      </main>
-    </Layout>
+          <div className='container-fluid'>
+            <div className='row'>
+              <div className='col-md-12'>{showAllBlogs()}</div>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </React.Fragment>
   );
 };
 
@@ -76,4 +115,4 @@ Blogs.getInitialProps = () => {
   });
 };
 
-export default Blogs;
+export default withRouter(Blogs);
