66ec3e00b841136b78059013d15166b5b5f055b2
pornpol <pornpol.w@gmail.com>
2020-03-20 20:57:31 +0700

    Add related blogs on single blog page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,15 +1,32 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import renderHTML from 'react-render-html';
 import moment from 'moment';
 import Layout from '../../components/Layout';
+import SmallCard from '../../components/blog/SmallCard';
 
-import { singleBlog } from '../../actions/blog';
+import { singleBlog, listRelated } from '../../actions/blog';
 
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 
 const SingleBlog = ({ blog, query }) => {
+  const [related, setRelated] = useState([]);
+
+  const loadRelated = () => {
+    listRelated({ blog }).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setRelated(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadRelated();
+  }, []);
+
   const head = () => (
     <Head>
       <title>
@@ -46,6 +63,16 @@ const SingleBlog = ({ blog, query }) => {
         <a className='btn btn-outline-primary mr-1 ml-1 mt-3'>{t.name}</a>
       </Link>
     ));
+
+  const showRelatedBlog = () => {
+    return related.map((blog, i) => (
+      <div className='col-md-4' key={i}>
+        <article>
+          <SmallCard blog={blog} />
+        </article>
+      </div>
+    ));
+  };
 
   return (
     <React.Fragment>
@@ -86,10 +113,10 @@ const SingleBlog = ({ blog, query }) => {
                 <div className='col-md-12 lead'>{renderHTML(blog.body)}</div>
               </section>
             </div>
-            <div className='container pb-5'>
+            <div className='container'>
               <h4 className='text-center pt-5 pb-5 h2'>Related blogs</h4>
               <hr />
-              <p>show related blogs</p>
+              <div className='row'>{showRelatedBlog()}</div>
             </div>
             <div className='container pb-5'>
               <p>show comments</p>
