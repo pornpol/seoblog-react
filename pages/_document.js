a9a398ae9ddfeeb7dd4a03b873cb958760ebcb1a
pornpol <pornpol.w@gmail.com>
2020-03-26 21:05:09 +0700

    Add google analytic

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
 class MyDocument extends Document {
+  setGoogleTags() {
+    if (publicRuntimeConfig.PRODUCTION) {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'UA-161956443-1');
+        `
+      };
+    }
+  }
+
   render() {
     return (
       <Html lang='en'>
@@ -15,6 +31,11 @@ class MyDocument extends Document {
             href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css'
           />
           <link rel='stylesheet' href='/static/css/style.css' />
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=UA-161956443-1'
+          ></script>
+          <script dangerouslySetInnerHTML={this.setGoogleTags()}></script>
         </Head>
         <body>
           <Main />
