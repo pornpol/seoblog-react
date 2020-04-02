266628bb75c75f917614ba4d0fb5059595ace311
pornpol <pornpol.w@gmail.com>
2020-04-02 18:10:00 +0700

    Update index.js for seo

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import Link from 'next/link';
-import {
-  UncontrolledCarousel,
-  Card,
-  Button,
-  CardImg,
-  CardTitle,
-  CardText,
-  CardColumns,
-  CardSubtitle,
-  CardBody
-} from 'reactstrap';
-import { DOMAIN } from '../config';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../config';
+import { UncontrolledCarousel } from 'reactstrap';
 
 const Index = () => {
   const items = [
@@ -32,7 +23,7 @@ const Index = () => {
     {
       src: `${DOMAIN}/static/images/header3.jpg`,
       altText: 'Slide 3',
-      caption: 'Traveller',
+      caption: 'Explorer',
       header: 'PORNPOL WASUWAT',
       key: '3'
     }
@@ -60,162 +51,201 @@ const Index = () => {
       key: '3'
     }
   ];
+
+  const head = () => (
+    <Head>
+      <title>Programming blogs | {APP_NAME}</title>
+      <meta
+        name='description'
+        content='Pornpol Wasuwat - Personal blogs and tutorials on react node next vue php laravel ans web development'
+      />
+      <link rel='canonical' href={`${DOMAIN}`} />
+      <meta
+        property='og:title'
+        content={`Pornpol Wasuwat Personal Web-site | ${APP_NAME}`}
+      />
+      <meta
+        property='og:description'
+        content='Pornpol Wasuwat - Personal blogs and tutorials on react node next vue php laravel ans web development'
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content={`${DOMAIN}`} />
+      <meta property='og:site_name' content={`${APP_NAME}`} />
+
+      <meta
+        property='og:image'
+        content={`${DOMAIN}/static/images/header1.jpg`}
+      />
+      <meta
+        property='og:image:secure_url'
+        content={`${DOMAIN}/static/images/header1.jpg`}
+      />
+      <meta property='og:image:type' content='image/jpg' />
+      <meta property='fb:app_id' content={`${FB_APP_ID}`} />
+    </Head>
+  );
+
   return (
-    <Layout>
-      <article className='overflow-hidden'>
-        <div className='container'>
-          <UncontrolledCarousel items={items} />
-        </div>
+    <React.Fragment>
+      {head()}
 
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12 pt-3'>
-              <p className='lead font-weight-bold'>ABOUT ME</p>
-              <p className='text-justify'>
-                Hi, My name is{' '}
-                <span className='font-weight-bold font-italic'>
-                  "Pornpol Wasuwat"
-                </span>
-                . You can call me{' '}
-                <span className='font-weight-bold font-italic'>"Pol"</span>. I
-                am Embedded IoT Engineer, Fullstack Developer, Runner and so on.
-                I fall in love with technology. I always try and learn new
-                things to make sure that the latest technologies are in my hand.
-              </p>
-              <p className='text-justify'>
-                I'm right here to support people to solve their problems with
-                cutting edge technologies. I have 10+ years experience in
-                industrial & agriculture sector. I participate in 50+ awesome
-                projects. If you have any problems. Let me take care.
-              </p>
-            </div>
+      <Layout>
+        <article className='overflow-hidden'>
+          <div className='container'>
+            <UncontrolledCarousel items={items} />
           </div>
-        </div>
 
-        <div className='container'>
-          <p className='lead font-weight-bold'>WORK EXPERIENCE</p>
-          <p>
-            <span className='font-weight-bold font-italic'>
-              Fullstack IoT Developer
-            </span>{' '}
-            - Freelance (Sep 19 - Present)
-            <br />
-            <span className='font-weight-bold font-italic'>
-              VP Hardware Engineering
-            </span>{' '}
-            - Energy Response Co., Ltd. (Aug 17 - Aug 19)
-            <br />
-            <span className='font-weight-bold font-italic'>
-              Product Manager
-            </span>{' '}
-            - MixEn Corporation Co., Ltd. (jul 12 - Jul 17)
-            <br />
-            <span className='font-weight-bold font-italic'>
-              Production Superviser
-            </span>{' '}
-            - Halcyon Technology Public Co., Ltd. (Mar 11 - Jun 12)
-            <br />
-            <span className='font-weight-bold font-italic'>
-              Embedded Engineer
-            </span>{' '}
-            - MixEn Corporation Co., Ltd. (Feb 07 - Jan 11)
-            <br />
-            <span className='font-weight-bold font-italic'>Programmer</span> -
-            Toyota Tsusho Electronics (Thailand) Co., Ltd. (Apr 06 - Jan 07)
-          </p>
-        </div>
-
-        <div className='container pb-4'>
-          <p className='lead font-weight-bold'>PORTFOLIOS</p>
-          <UncontrolledCarousel items={items2} />
-        </div>
-
-        <div className='container'>
-          <p className='lead font-weight-bold'>BLOGS</p>
-          <div className='row'>
-            <div className='col-md-4'>
-              <div className='flip flip-horizontal'>
-                <div
-                  className='front'
-                  style={{
-                    backgroundImage:
-                      'url(' +
-                      'https://images.pexels.com/photos/34140/pexels-photo.jpg' +
-                      ')'
-                  }}
-                >
-                  <h2 className='text-shadow text-center h1'>React</h2>
-                </div>
-                <div className='back text-center'>
-                  <Link href='/categories/react'>
-                    <a>
-                      <h3 className='h1'>React Js</h3>
-                    </a>
-                  </Link>
-                  <p className='lead'>
-                    The world's most popular frontend web development library
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-4'>
-              <div className='flip flip-horizontal'>
-                <div
-                  className='front'
-                  style={{
-                    backgroundImage:
-                      'url(' +
-                      'https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg' +
-                      ')'
-                  }}
-                >
-                  <h2 className='text-shadow text-center h1'>NodeJs</h2>
-                </div>
-                <div className='back text-center'>
-                  <Link href='/categories/nodejs'>
-                    <a>
-                      <h3 className='h1'>Node Js</h3>
-                    </a>
-                  </Link>
-                  <p className='lead'>
-                    The worlds most popular backend development tool for
-                    JavaScript Ninjas
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-4'>
-              <div className='flip flip-horizontal'>
-                <div
-                  className='front'
-                  style={{
-                    backgroundImage:
-                      'url(' +
-                      'https://images.pexels.com/photos/257736/pexels-photo-257736.jpeg' +
-                      ')'
-                  }}
-                >
-                  <h2 className='text-shadow text-center h1'>IoT</h2>
-                </div>
-                <div className='back text-center'>
-                  <Link href='/categories/iot'>
-                    <a>
-                      <h3 className='h1'>IoT</h3>
-                    </a>
-                  </Link>
-                  <p className='lead'>
-                    The internet of things is the game-changer for an overall
-                    business ecosystem transformation.
-                  </p>
-                </div>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-12 pt-3'>
+                <p className='lead font-weight-bold'>ABOUT ME</p>
+                <p className='text-justify'>
+                  Hi, My name is{' '}
+                  <span className='font-weight-bold font-italic'>
+                    "Pornpol Wasuwat"
+                  </span>
+                  . You can call me{' '}
+                  <span className='font-weight-bold font-italic'>"Pol"</span>. I
+                  am Embedded IoT Engineer, Fullstack Developer, Runner and so
+                  on. I fall in love with technology. I always try and learn new
+                  things to make sure that the latest technologies are in my
+                  hand.
+                </p>
+                <p className='text-justify'>
+                  I'm right here to support people to solve their problems with
+                  cutting edge technologies. I have 10+ years experience in
+                  industrial & agriculture sector. I participate in 50+ awesome
+                  projects. If you have any problems. Let me take care.
+                </p>
               </div>
             </div>
           </div>
-        </div>
-      </article>
-    </Layout>
+
+          <div className='container'>
+            <p className='lead font-weight-bold'>WORK EXPERIENCE</p>
+            <p>
+              <span className='font-weight-bold font-italic'>
+                Fullstack IoT Developer
+              </span>{' '}
+              - Freelance (Sep 19 - Present)
+              <br />
+              <span className='font-weight-bold font-italic'>
+                VP Hardware Engineering
+              </span>{' '}
+              - Energy Response Co., Ltd. (Aug 17 - Aug 19)
+              <br />
+              <span className='font-weight-bold font-italic'>
+                Product Manager
+              </span>{' '}
+              - MixEn Corporation Co., Ltd. (jul 12 - Jul 17)
+              <br />
+              <span className='font-weight-bold font-italic'>
+                Production Superviser
+              </span>{' '}
+              - Halcyon Technology Public Co., Ltd. (Mar 11 - Jun 12)
+              <br />
+              <span className='font-weight-bold font-italic'>
+                Embedded Engineer
+              </span>{' '}
+              - MixEn Corporation Co., Ltd. (Feb 07 - Jan 11)
+              <br />
+              <span className='font-weight-bold font-italic'>Programmer</span> -
+              Toyota Tsusho Electronics (Thailand) Co., Ltd. (Apr 06 - Jan 07)
+            </p>
+          </div>
+
+          <div className='container pb-4'>
+            <p className='lead font-weight-bold'>PORTFOLIOS</p>
+            <UncontrolledCarousel items={items2} />
+          </div>
+
+          <div className='container'>
+            <p className='lead font-weight-bold'>BLOGS</p>
+            <div className='row'>
+              <div className='col-md-4'>
+                <div className='flip flip-horizontal'>
+                  <div
+                    className='front'
+                    style={{
+                      backgroundImage:
+                        'url(' +
+                        'https://images.pexels.com/photos/34140/pexels-photo.jpg' +
+                        ')'
+                    }}
+                  >
+                    <h2 className='text-shadow text-center h1'>React</h2>
+                  </div>
+                  <div className='back text-center'>
+                    <Link href='/categories/react'>
+                      <a>
+                        <h3 className='h1'>React Js</h3>
+                      </a>
+                    </Link>
+                    <p className='lead'>
+                      The world's most popular frontend web development library
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-md-4'>
+                <div className='flip flip-horizontal'>
+                  <div
+                    className='front'
+                    style={{
+                      backgroundImage:
+                        'url(' +
+                        'https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg' +
+                        ')'
+                    }}
+                  >
+                    <h2 className='text-shadow text-center h1'>NodeJs</h2>
+                  </div>
+                  <div className='back text-center'>
+                    <Link href='/categories/nodejs'>
+                      <a>
+                        <h3 className='h1'>Node Js</h3>
+                      </a>
+                    </Link>
+                    <p className='lead'>
+                      The worlds most popular backend development tool for
+                      JavaScript Ninjas
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-md-4'>
+                <div className='flip flip-horizontal'>
+                  <div
+                    className='front'
+                    style={{
+                      backgroundImage:
+                        'url(' +
+                        'https://images.pexels.com/photos/257736/pexels-photo-257736.jpeg' +
+                        ')'
+                    }}
+                  >
+                    <h2 className='text-shadow text-center h1'>IoT</h2>
+                  </div>
+                  <div className='back text-center'>
+                    <Link href='/categories/iot'>
+                      <a>
+                        <h3 className='h1'>IoT</h3>
+                      </a>
+                    </Link>
+                    <p className='lead'>
+                      The internet of things is the game-changer for an overall
+                      business ecosystem transformation.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+      </Layout>
+    </React.Fragment>
   );
 };
 
