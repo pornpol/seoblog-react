b498983e098959f03cbc084940137d445950803a
pornpol <pornpol.w@gmail.com>
2020-04-02 17:36:44 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,52 +39,25 @@ const Index = () => {
   ];
   const items2 = [
     {
-      src:
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-      altText: 'Slide 1',
-      caption: 'Slide 1',
-      header: 'Slide 1 Header',
+      src: `${DOMAIN}/static/images/portfolios/project1.jpg`,
+      altText: 'IoT Environment Monitor',
+      caption: 'Remote enviroment real-time monitor',
+      header: 'IoT Environment Monitor',
       key: '1'
     },
     {
-      src:
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-      altText: 'Slide 2',
-      caption: 'Slide 2',
-      header: 'Slide 2 Header',
+      src: `${DOMAIN}/static/images/portfolios/project2.jpg`,
+      altText: 'Production Monitoring System',
+      caption: 'Real-time production monitor',
+      header: 'Production Monitoring System',
       key: '2'
     },
     {
-      src:
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-      altText: 'Slide 3',
-      caption: 'Slide 3',
-      header: 'Slide 3 Header',
+      src: `${DOMAIN}/static/images/portfolios/project3.jpg`,
+      altText: 'IoT Power Monitoring System',
+      caption: 'Remote power real-time monitor',
+      header: 'IoT Power Monitoring System',
       key: '3'
-    },
-    {
-      src:
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-      altText: 'Slide 4',
-      caption: 'Slide 4',
-      header: 'Slide 4 Header',
-      key: '4'
-    },
-    {
-      src:
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-      altText: 'Slide 5',
-      caption: 'Slide 5',
-      header: 'Slide 5 Header',
-      key: '5'
-    },
-    {
-      src:
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-      altText: 'Slide 6',
-      caption: 'Slide 6',
-      header: 'Slide 6 Header',
-      key: '6'
     }
   ];
   return (
@@ -158,7 +131,7 @@ const Index = () => {
         </div>
 
         <div className='container'>
-          <p className='lead font-weight-bold'>RECENT BLOGS</p>
+          <p className='lead font-weight-bold'>BLOGS</p>
           <div className='row'>
             <div className='col-md-4'>
               <div className='flip flip-horizontal'>
@@ -167,7 +140,7 @@ const Index = () => {
                   style={{
                     backgroundImage:
                       'url(' +
-                      'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
+                      'https://images.pexels.com/photos/34140/pexels-photo.jpg' +
                       ')'
                   }}
                 >
@@ -193,14 +166,14 @@ const Index = () => {
                   style={{
                     backgroundImage:
                       'url(' +
-                      'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
+                      'https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg' +
                       ')'
                   }}
                 >
-                  <h2 className='text-shadow text-center h1'>Node</h2>
+                  <h2 className='text-shadow text-center h1'>NodeJs</h2>
                 </div>
                 <div className='back text-center'>
-                  <Link href='/categories/node'>
+                  <Link href='/categories/nodejs'>
                     <a>
                       <h3 className='h1'>Node Js</h3>
                     </a>
@@ -220,20 +193,21 @@ const Index = () => {
                   style={{
                     backgroundImage:
                       'url(' +
-                      'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
+                      'https://images.pexels.com/photos/257736/pexels-photo-257736.jpeg' +
                       ')'
                   }}
                 >
-                  <h2 className='text-shadow text-center h1'>Next</h2>
+                  <h2 className='text-shadow text-center h1'>IoT</h2>
                 </div>
                 <div className='back text-center'>
-                  <Link href='/categories/nextjs'>
+                  <Link href='/categories/iot'>
                     <a>
-                      <h3 className='h1'>Next Js</h3>
+                      <h3 className='h1'>IoT</h3>
                     </a>
                   </Link>
                   <p className='lead'>
-                    A Production ready web framework for building SEO React apps
+                    The internet of things is the game-changer for an overall
+                    business ecosystem transformation.
                   </p>
                 </div>
               </div>
